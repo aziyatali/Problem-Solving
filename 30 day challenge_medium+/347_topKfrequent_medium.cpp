4200e894c727f013f9337e71a8e7bebc37709e39
Aziyat <aziyatalik4@gmail.com>
2021-05-27 16:19:00 +0900

    solved top k frequent

--- 30 day challenge_medium+/347_topKfrequent_medium.cpp
+++ 30 day challenge_medium+/347_topKfrequent_medium.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int>ans;
+    map<int, int>mp;
+    for (int i = 0; i < nums.size(); i++){
+        mp[nums[i]]++;
+    }
         
+    priority_queue<pair<int, int>>pq;
+    for (auto it = mp.begin(); it != mp.end(); it++){
+        pq.push(make_pair(it->second, it->first));
+        
+        if (pq.size() > mp.size()-k){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+    }
+        return ans;
     }
 };
