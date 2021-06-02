fa1afdc63279f6ee20a8db2778f58d8e142c6259
Aziyat <aziyatalik4@gmail.com>
2021-06-02 17:22:50 +0900

    solved 4sum for o(n^2)

--- 30 day challenge_medium+/18_4sum_medium.cpp
+++ 30 day challenge_medium+/18_4sum_medium.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        unordered_map<int, int>hash;
+        if (nums.size() < 4){
+            return {};
+        }
+        sort(nums.begin(), nums.end());
         vector<vector<int>>res;
-        vector<int>ans;
-        for (int i = 0; i < nums.size(); i++){
-            int threeSum = target - nums[i];
-            int j = i+1;
-            while (j < nums.size()-1){
-                int twoSum = threeSum-nums[j];
-                int tar = twoSum - nums[j+1];
-                auto it = hash.find(tar);
-                if (it != hash.end() && it->first != i && it->first != j){
-                    ans.clear();
-                    ans.push_back(nums[i]);
-                    ans.push_back(nums[j]);
-                    ans.push_back(nums[j+1]);
-                    ans.push_back(nums[it->second]);
-                    res.push_back(ans);
+        for (int i = 0; i < nums.size()-1; i++){
+            for (int j = i+1; j < nums.size(); j++){
+                int twoSum = target - (nums[i] + nums[j]);
+                int left = j+1, right = nums.size()-1;
+                while(left < nums.size() && left < right){
+                    vector<int>quad(4, 0);
+
+                    int tar = nums[left] + nums[right];
+                    if (tar > twoSum){
+                        right--;
+                    }
+                    else if (tar < twoSum){
+                        left++;
+                    }
+                    else{
+                        quad[0] = nums[i];
+                        quad[1] = nums[j];
+                        quad[2] = nums[left];
+                        quad[3] = nums[right];
+                        res.push_back(quad);
+                        while(left < nums.size()-1 && quad[2] == nums[left])left++;
+                        while(right > left+1 && quad[3] == nums[right])right--;
+
+                    }                    
                 }
-                j++;
-            }
-            hash[i] = nums[i];
+                while(j < nums.size()-1 && nums[j] == nums[j+1])j++;
+                        }
+            while(i < nums.size()-1 && nums[i] == nums[i+1])i++;
+            
         }
         
         return res;
