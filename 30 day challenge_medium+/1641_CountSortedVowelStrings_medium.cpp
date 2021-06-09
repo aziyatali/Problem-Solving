98c15c625f2a7f1ef06ff943f310888796596c1e
Aziyat <aziyatalik4@gmail.com>
2021-06-10 04:03:13 +0900

    added revised solution

--- 30 day challenge_medium+/1641_CountSortedVowelStrings_medium.cpp
+++ 30 day challenge_medium+/1641_CountSortedVowelStrings_medium.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        vector<vector<int>>dp(n+1, vector<int>(6));
+        int dp[] = {0,1,1,1,1,1};
         for (int i = 1; i <= n; i++){
             for (int k = 1; k < 6; k++){
-                dp[i][k] = dp[i][k-1] + (i > 1 ? dp[i-1][k] : 1);
+            dp[k]+=dp[k-1];
             }
         }
-        return dp[n][5];
+        return dp[5];
+        // dp[2] = dp[2]+dp[1];
+        //3         2    1
+        //6         3    3
+        //10        4    6
+        //15        5    10
     }
 };
