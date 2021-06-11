e0b0740639772792015806e245163e7feb025921
Aziyat <aziyatalik4@gmail.com>
2021-06-11 13:34:13 +0900

    solved Combinations

--- 30 day challenge_medium+/77_Combinations_medium.cpp
+++ 30 day challenge_medium+/77_Combinations_medium.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>p(k, 0);
+        int i = 0; 
+        while (i  >= 0){
+           p[i]++;
+            if (p[i] > n){
+                i--;
+            }else if (i == k-1){
+                ans.push_back(p);
+            }else{
+                i++;
+                p[i] = p[i-1];
+            }
+        }
+        return ans;
         
     }
 };
