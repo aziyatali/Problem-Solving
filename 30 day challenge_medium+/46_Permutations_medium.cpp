7c7848dac75831907fc566d02762967a5063f1f8
Aziyat <aziyatalik4@gmail.com>
2021-06-12 21:43:57 +0900

    solved with recurive

--- 30 day challenge_medium+/46_Permutations_medium.cpp
+++ 30 day challenge_medium+/46_Permutations_medium.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        
+        vector<vector<int>>res;
+        recPerm(nums, 0, res);
+        return res;
+    }
+    void recPerm(vector<int>nums, int beg, vector<vector<int>>&res){
+        if (beg >= nums.size()){
+            res.push_back(nums);
+            return;
+        }
+        for (int i = beg; i < nums.size(); i++){
+            // change for each case
+            swap(nums[beg], nums[i]);
+            // change the changed case
+            recPerm(nums, beg+1, res);
+            // reset the previus values
+            swap(nums[beg], nums[i]);
+        }
     }
 };
