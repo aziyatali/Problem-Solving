1856ca78e17570acfcbc6d9f884c426e27703b74
Aziyat <aziyatalik4@gmail.com>
2021-06-13 13:35:51 +0900

    solved Comb sum

--- 30 day challenge_medium+/39_CombinationSum_medium.cpp
+++ 30 day challenge_medium+/39_CombinationSum_medium.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>combination;
+        vector<vector<int>>res;
+        sort(candidates.begin(), candidates.end());
+        combSum(candidates, combination, target, res, 0);
+        return res;
+    }
+    
+    void combSum (vector<int>&cand,vector<int>&comb, int target, vector<vector<int>>&res, int beg){
+        if (target == 0){
+            res.push_back(comb);
+        }
         
+        for (int i = beg; i < cand.size() && target >= cand[i]; i++){
+            comb.push_back(cand[i]);
+            combSum(cand, comb, target-cand[i], res, i);
+            comb.pop_back();
+        }
     }
 };
