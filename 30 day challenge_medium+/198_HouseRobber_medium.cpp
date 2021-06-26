0d57deb67d8b5c3436401560ebbaf4afabaf2655
Aziyat <aziyatalik4@gmail.com>
2021-06-27 05:44:43 +0900

    added buggy solution

--- 30 day challenge_medium+/198_HouseRobber_medium.cpp
+++ 30 day challenge_medium+/198_HouseRobber_medium.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-//         Time limit exceeds
-        return robber(nums, nums.size()-1);
-    }
-    int robber(vector<int>& nums, int i){
-        if ( i < 0){
-            return 0;
+        if (nums.size() == 1){
+            return nums[0];
         }
-        return max(robber(nums, i-2)+nums[i], robber(nums, i-1));
+        if(nums.size() == 2){
+            return max(nums[0], nums[1]);
+        }
+        vector<int>ans(nums.size()+1, 0);
+        ans[0] = nums[0];
+        ans[1] = nums[1];
+        for (int i = 2; i < nums.size(); i++){
+            ans[i] = max(ans[i-2]+nums[i], ans[i-1]);
+        }
+        return ans[nums.size()-1];
     }
 };
