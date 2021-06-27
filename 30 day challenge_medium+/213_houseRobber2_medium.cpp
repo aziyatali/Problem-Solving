6570f7214c0c74238e0d5a583d68f3560d0588a2
Aziyat <aziyatalik4@gmail.com>
2021-06-27 21:54:17 +0900

    solved house robber 2

--- 30 day challenge_medium+/213_houseRobber2_medium.cpp
+++ 30 day challenge_medium+/213_houseRobber2_medium.cpp
@@ -5,20 +5,17 @@ public:
             return nums[0];
         }
         vector<int>left(nums.size(), 0);
-        vector<int>right(nums.size()+1, 0);
+        vector<int>right(nums.size(), 0);
         left[0] = 0;
         left[1] = nums[0];
-        right[nums.size()] = 0;
-        right[nums.size()-1] = nums[nums.size()-1];
+        right[0] = 0;
+        right[1] = nums[1];
         
         for(int i = 1; i < nums.size()-1; i++){
             left[i+1] = max(left[i-1]+nums[i], left[i]);
+            right[i+1] = max(right[i-1]+nums[i+1], right[i]);
         }
-        
-        for (int j = nums.size()-1; j > 1; j--){
-            right[j-1] = max(right[j+1]+nums[j], right[j]);
-        }
-        return max(left[nums.size()-1], right[1]);
+        return max(left[nums.size()-1], right[nums.size()-1]);
         
     }
 };
