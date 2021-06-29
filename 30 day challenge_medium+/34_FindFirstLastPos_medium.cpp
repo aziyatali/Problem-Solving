42484b9b970ca30bdcbc0da77446c2cf0383cfa5
Aziyat <aziyatalik4@gmail.com>
2021-06-29 23:26:14 +0900

    added buggy solution

--- 30 day challenge_medium+/34_FindFirstLastPos_medium.cpp
+++ 30 day challenge_medium+/34_FindFirstLastPos_medium.cpp
@@ -2,20 +2,34 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
       int left = 0, right = nums.size()-1;
-       vector<int>ans = {-1, -1};
-        while(left <= right){
-            int mid = (right-left)/2 + left;
-            if (target>=nums[left]){
+       vector<int>ans(2, -1);
+        while(left < right){
+            int mid = (right+left)/2;
+            if (target > nums[mid]){
                 left = mid+1; 
             }
             else{
-                right = mid-1;
+                right = mid;
             }
         }
-        if (ans[left] == target){
-            
-        ans[0] = left;
-        ans[1] = left+1;   
+        
+        if (nums[left] == target){
+            ans[0] = left;
+        }
+        
+        right = nums.size()-1;
+        
+        while (left < right){
+            int mid  = (right+left)/2 + 1;
+            if (nums[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid;
+            }
+        }
+        
+        if (ans[right] == target){
+            ans[1] = right;   
         }
         return ans;
             
