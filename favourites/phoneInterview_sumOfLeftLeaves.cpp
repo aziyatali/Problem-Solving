d08dfbbe7b73b9a50596f660c593e97fd4d6eccd
Aziyat Murzamidinov <aziyatalik4@gmail.com>
2021-08-25 19:11:44 +0900

    solved sum of left leaves

--- favourites/phoneInterview_sumOfLeftLeaves.cpp
+++ favourites/phoneInterview_sumOfLeftLeaves.cpp
@@ -1,1 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        getNodes(root, sum);
+        return sum;
+    }
+    void getNodes(TreeNode * root, int &sum){
+        if (!root){
+            return;
+        }
+        if(root->left && !root->left->left && !root->left->right){
+            sum+=root->left->val;
+        }
+        getNodes(root->left, sum);
+        getNodes(root->right, sum);
+    }
+};
