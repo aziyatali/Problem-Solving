d26b675edd1e27cc3ebfa76c131902685300ca48
Aziyat Murzamidinov <aziyatalik4@gmail.com>
2021-09-06 18:08:47 +0900

    Update 1010_pairOfSongs_medium.cpp

--- favourites/1010_pairOfSongs_medium.cpp
+++ favourites/1010_pairOfSongs_medium.cpp
@@ -10,3 +10,18 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int numPairsDivisibleBy60(vector<int>& time) {
+//         unordered_map<int, int>umap;
+//         int ans = 0;
+//         for (auto t: time){
+//             auto it = umap.find((600-t)%60);
+//             if (it!=umap.end())
+//                 ans+=it->second;
+//             umap[t%60]++;
+//         }
+//         return ans;
+//     }
+// };
