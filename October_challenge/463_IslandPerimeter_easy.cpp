6a54213a7c26546d18f44f54814891c2ad115ab6
Aziyat Murzamidinov <aziyatalik4@gmail.com>
2021-10-04 14:05:54 +0900

    Update 463_IslandPerimeter_easy.cpp

--- October_challenge/463_IslandPerimeter_easy.cpp
+++ October_challenge/463_IslandPerimeter_easy.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int sum = 0;
+        int count = 0, repeat = 0;
         for (int i = 0; i < grid.size(); i++){
             for (int j = 0; j < grid[0].size(); j++){
                 if (grid[i][j] == 1){
-                    // EDGE CASES
-                    if (i-1 < 0)
-                        sum++;
-                    if (j+1 >= grid[0].size())
-                        sum++;
-                    if (i+1 >= grid.size())
-                        sum++;
-                    if (j-1 < 0)
-                        sum++;
-                    // ZEROS CASES
-                    if (i-1 >= 0 && grid[i-1][j] == 0)
-                        sum++;
-                    if (i+1 < grid.size() && grid[i+1][j] == 0)
-                        sum++;
-                    if (j-1 >= 0 && grid[i][j-1] == 0)
-                        sum++;
-                    if (j+1 < grid[0].size() && grid[i][j+1] == 0)
-                        sum++;
-
+                    count++;
+                    if (i!= 0 && grid[i-1][j] == 1)
+                        repeat++;
+                    if (j!= 0 && grid[i][j-1] == 1)
+                        repeat++;
                 }
+            
             }
         }
-        return sum;
+        // If two grids are connected their perimeter subtracted by two. 
+        return count*4 - repeat*2;
     }
 };
